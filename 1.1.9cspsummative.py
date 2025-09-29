df64e354550bae13bd87647777c30d234fe16b00
calebrenn <234990@shakopeeschools.org>
2025-09-29 11:16:08 -0500

    modified:   1.1.9cspsummative.py

--- 1.1.9cspsummative.py
+++ 1.1.9cspsummative.py
@@ -1,3 +1,50 @@
+import turtle as trtl
+tree = trtl.Turtle()
+tree.speed(0)
+
+#configure the trunk of the tree
+tree.pensize(3)
+tree.pencolor("brown")
+
+# build the trunk of the tree
+tree.penup()
+tree.goto(60, -380)
+tree.pendown()
+tree.fillcolor("brown")
+tree.begin_fill()
+tree.left(90)
+tree.forward(380)
+tree.left(90)
+tree.forward(120)
+tree.left(90)
+tree.forward(380)
+tree.fillcolor("brown")
+tree.end_fill()
+
+# configure the color of the leaves
+color = trtl.textinput("color", "What color do you want the leaves to be?")
+tree.pencolor(color)
+
+# build the leaves of the tree
+tree.penup()
+tree.fillcolor(color)
+tree.begin_fill()
+tree.goto(0, -70)
+tree.left(90)
+tree.pendown()
+tree.circle(225, 360)
+tree.end_fill()
 
 
-# build the stem of the tree
+
+
+
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
